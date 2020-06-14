9ee08e3b2de1d043b7ddb1ab23082c1640d9e460
Nikita Shikuts <shikutsn@bk.ru>
2020-06-14 23:40:35 +0300

    Строит список фильмов на основе наших данных.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,13 +4,14 @@ import Main from "../main/main.jsx";
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {promoMovieTitle, promoMovieGenre, promoMovieReleaseYear} = props;
+  const {promoMovieTitle, promoMovieGenre, promoMovieReleaseYear, films} = props;
 
   return (
     <Main
       promoMovieTitle = {promoMovieTitle}
       promoMovieGenre = {promoMovieGenre}
       promoMovieReleaseYear = {promoMovieReleaseYear}
+      films = {films}
     />
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-const promoMovie = {
-  title: `The Grand Budapest Hotel`,
-  genre: `Drama`,
-  releaseYear: 2014,
+const AppData = {
+  PROMO_MOVIE: {
+    TITLE: `The Grand Budapest Hotel`,
+    GENRE: `Drama`,
+    RELEASE_YEAR: 2014,
+  },
+  FILMS: [`Revenant`, `No Country for Old Men`, `Snatch`, `Bohemian Rhapsody`, `Pulp Fiction`, `Shutter Island`],
 };
 
 ReactDOM.render(
     <App
-      promoMovieTitle = {promoMovie.title}
-      promoMovieGenre = {promoMovie.genre}
-      promoMovieReleaseYear = {promoMovie.releaseYear}
+      promoMovieTitle = {AppData.PROMO_MOVIE.TITLE}
+      promoMovieGenre = {AppData.PROMO_MOVIE.GENRE}
+      promoMovieReleaseYear = {AppData.PROMO_MOVIE.RELEASE_YEAR}
+      films = {AppData.FILMS}
     />,
     document.querySelector(`#root`)
 );
